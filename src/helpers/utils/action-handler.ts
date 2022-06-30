1aeb55f8673d925e5ede98eb1062deab5e6e85ad
Yanis Dib <ynsprive@gmail.com>
2022-06-30 19:46:00 +0200

    Update action-handler.ts

--- src/helpers/utils/action-handler.ts
+++ src/helpers/utils/action-handler.ts
@@ -3,9 +3,8 @@ import { AnyAction } from '@reduxjs/toolkit';
 import { FailedAction } from '../../interfaces/FailedAction';
 
 
-/**
- * Handle failed actions dispatched to the Redux store
- */ 
-export const isFailedAction = (action: AnyAction): action is FailedAction => {
+export const isFailedAction = (
+    action: AnyAction
+): action is FailedAction => {
     return action.type.endsWith('Failure');
 };
