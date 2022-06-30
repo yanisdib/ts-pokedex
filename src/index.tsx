4a30efa030bfadb4f9c608f1a095f1e61bdca459
Yanis Dib <ynsprive@gmail.com>
2022-06-30 21:44:42 +0200

    Add store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider as StoreProvider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import store from './services/redux/store';
 
 import App from './App';
 
@@ -12,8 +15,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <StoreProvider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </StoreProvider>
   </React.StrictMode>
 );
