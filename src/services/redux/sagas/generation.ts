0868ae5ebcae7a1cbf6f62d75fab0ade3da01bbb
Yanis Dib <ynsprive@gmail.com>
2022-06-30 20:04:10 +0200

    Create generation saga

---
+++ src/services/redux/sagas/generation.ts
@@ -0,0 +1,21 @@
+import { call, put, takeLeading } from '@redux-saga/core/effects';
+import { PayloadAction } from '@reduxjs/toolkit';
+
+import { Generation } from '../../../interfaces/Generation';
+import { fetchGeneration } from '../../api/generation';
+import { fetchGenerationRequest, fetchGenerationSuccess } from '../slices/generation';
+
+
+export function* fetchGenerationSaga() {
+    try {
+        const generationList: Generation[] = yield call(fetchGeneration);
+        yield put(fetchGenerationSuccess(generationList));
+    } catch {
+        const error: Error = new Error('Something went wrong.');
+        yield put({ type: 'generation/fetchGenerationFailure', error });
+    }
+}
+
+export function* generationSaga() {
+    yield takeLeading(fetchGenerationRequest, fetchGenerationSaga);
+}
