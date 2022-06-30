45a98f8f35c3abddbf83ca684b0da6e9cb5444be
Yanis Dib <ynsprive@gmail.com>
2022-06-30 19:33:53 +0200

    Create utils to handle failed actions

---
+++ src/helpers/action-handler.ts
@@ -0,0 +1,11 @@
+import { AnyAction } from '@reduxjs/toolkit';
+
+import { FailedAction } from '../interfaces/FailedAction';
+
+
+/**
+ * Handle failed actions dispatched to the Redux store
+ */ 
+export const isFailedAction = (action: AnyAction): action is FailedAction => {
+    return action.type.endsWith('Failure');
+};
