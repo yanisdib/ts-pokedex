baa6c1a8514e178b74de3472613ca6e9eb1548bd
Yanis Dib <ynsprive@gmail.com>
2022-07-01 14:52:13 +0200

    Create export index

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Container } from './Container/Container';
+export { default as Loading } from './Loading/Loading';
+export { default as Spinner } from './Spinner/Spinner';
+export { default as Tag } from './Tag/Tag';
