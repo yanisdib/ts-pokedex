ebc74c7534e6aa6553014e877a979b626afba942
Yanis Dib <ynsprive@gmail.com>
2022-07-01 14:12:42 +0200

    Implement theme and dark mode providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import { Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import { useDarkMode } from './hooks/useDarkMode';
+import { DarkModeContext } from './contexts/DarkModeContext';
+
+import Global from './assets/styles/global';
+import { darkTheme, defaultTheme } from './assets/styles/theme';
 
 import Home from './pages/Home/Home';
 
 
-function App() {
+function App(): JSX.Element {
+  const [isDarkModeEnabled, setDarkMode] = useDarkMode();
+
   return (
-    <main>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </main>
+    <ThemeProvider theme={isDarkModeEnabled ? darkTheme : defaultTheme}>
+      <Global />
+      <DarkModeContext.Provider value={setDarkMode}>
+        <main>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </main>
+      </DarkModeContext.Provider>
+    </ThemeProvider>
   );
 }
 
 
-export default App;
+export default App; 
