e4152b771c2abc715f1c4df4100b719c3d63621f
Yanis Dib <ynsprive@gmail.com>
2022-07-01 19:52:06 +0200

    Create test suites' for Hero component

---
+++ src/components/Hero/__tests__/Hero.test.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react'
+import Hero from '../Hero'
+
+describe('Hero component', () => {
+    test('should render without crashing', () => {
+        const { container } = render(<Hero />);
+
+        expect(container).toMatchSnapshot();
+    });
+});
