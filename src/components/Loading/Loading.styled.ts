632483d8689ad171b1427fdd9b8e2eb3f75582c0
Yanis Dib <ynsprive@gmail.com>
2022-07-01 14:51:35 +0200

    Create styled components for Loading

---
+++ src/components/Loading/Loading.styled.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+
+export const StyledLoading = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    min-height: 404px;
+    height: inherit;
+    background-color: var(--ui-theme-background);
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+`;
