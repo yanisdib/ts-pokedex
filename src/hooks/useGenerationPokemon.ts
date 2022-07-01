0c610b8bcc4c5f54ed52cfb1a89b4694d3ddf6e3
Yanis Dib <ynsprive@gmail.com>
2022-07-01 14:16:43 +0200

    Create useGenerationPokemon custom hook

---
+++ src/hooks/useGenerationPokemon.ts
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux'
+
+import { Generation } from '../interfaces/Generation';
+import { StoreState } from '../interfaces/StoreState';
+
+import { fetchGenerationRequest } from '../services/redux/slices/generation';
+
+import { useAppSelector } from './useAppSelector';
+
+
+export const useGenerationPokemon = (): StoreState<Generation[]> => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchGenerationRequest())
+    }, []);
+
+    return useAppSelector(state => state.generation);
+}
