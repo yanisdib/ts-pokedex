b03903bc13044c7980085559a43d086020a8e68d
Yanis Dib <ynsprive@gmail.com>
2022-07-01 16:10:53 +0200

    Add component exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,7 @@ export { default as Container } from './Container/Container';
 export { default as Loading } from './Loading/Loading';
 export { default as Spinner } from './Spinner/Spinner';
 export { default as Tag } from './Tag/Tag';
+
+export { default as Hero } from './Hero/Hero';
+
+export { default as ThemeSwitch } from './ThemeSwitch/ThemeSwitch';
