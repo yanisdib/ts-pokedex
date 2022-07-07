92d91b12daf761c0f1db4e0391aca0d7cc08fa0f
Yanis Dib <ynsprive@gmail.com>
2022-07-07 21:02:25 +0200

    Fix comments

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -6,16 +6,15 @@ type ReturnedValue = readonly [
     () => void
 ];
 
-
+/**
+ * Hooks that enables/disables a dark mode theme
+ * TODO: Manage saving user's choice with cookie creation and redux state
+ */
 export const useDarkMode = (
     initialValue?: boolean
 ): ReturnedValue => {
     const [isEnabled, setIsEnabled] = useState(initialValue ?? false);
 
-    /**
-     * Hooks that enables/disables a dark mode theme
-     * TODO: Manage saving user's choice with cookie creation and redux state
-     */
     const handleToggleMode = () => {
         setIsEnabled(previousState => !previousState)
     };
