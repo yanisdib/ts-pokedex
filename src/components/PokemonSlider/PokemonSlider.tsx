44abbdbe2f2ddd28b13bc8195ba113175deeb72a
Yanis Dib <ynsprive@gmail.com>
2022-07-08 00:44:37 +0200

    Create PokemonSlider component

---
+++ src/components/PokemonSlider/PokemonSlider.tsx
@@ -0,0 +1,34 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import { Pokemon } from '../../interfaces/Pokemon';
+
+import { Card, PokemonPreview, Slider } from '..';
+
+import { StyledPokemonSlider, Wrapper } from './PokemonSlider.styled';
+
+
+interface Props {
+    pokemon: Pokemon[];
+}
+
+
+function PokemonSlider({ pokemon }: Props): JSX.Element {
+    return (
+        <StyledPokemonSlider>
+            <Wrapper>
+                <Slider>
+                    {pokemon
+                        .map(element => (
+                            <Card>
+                                <PokemonPreview key={uuidv4()} pokemon={element} />
+                            </Card>
+                        ))
+                    }
+                </Slider>
+            </Wrapper>
+        </StyledPokemonSlider>
+    );
+}
+
+
+export default PokemonSlider;
