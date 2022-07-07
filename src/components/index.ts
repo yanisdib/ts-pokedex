7d8c5597f4e0e6aaf4bbc314051554a5c28914a9
Yanis Dib <ynsprive@gmail.com>
2022-07-08 00:44:04 +0200

    Add exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,14 @@
+export { default as Card } from './Card/Card';
 export { default as Container } from './Container/Container';
 export { default as Loading } from './Loading/Loading';
+export { default as NavigationButton } from './NavigationButton/NavigationButton';
+export { default as ProgresBar } from './ProgressBar/ProgressBar';
+export { default as Slider } from './Slider/Slider';
 export { default as Spinner } from './Spinner/Spinner';
 export { default as Tag } from './Tag/Tag';
 
 export { default as Hero } from './Hero/Hero';
+export { default as PokemonPreview } from './PokemonPreview/PokemonPreview';
+export { default as PokemonSlider } from './PokemonSlider/PokemonSlider';
 
 export { default as ThemeSwitch } from './ThemeSwitch/ThemeSwitch';
