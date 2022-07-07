64a0ee2351cd08e97837f6f5ae06b3951e4147d9
Yanis Dib <ynsprive@gmail.com>
2022-07-08 00:44:25 +0200

    Create styled components for PokemonSlider

---
+++ src/components/PokemonSlider/PokemonSlider.styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+
+export const StyledPokemonSlider = styled.div`
+    display: flex;
+    width: 100%;
+    height: fit-content;
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    width: inherit;
+    height: inherit;
+`;
