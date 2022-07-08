2b1d61f61c5c153513f2289c78a564b06eaeaca4
Yanis Dib <ynsprive@gmail.com>
2022-07-08 12:32:36 +0200

    Add navigation logic
    - Handle navigation buttons' logic
    - Handle slider's animation

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import NavigationButton from '../NavigationButton/NavigationButton';
 
-import { Content, StyledSlider, Wrapper } from './Slider.styled';
+import { Content, Navigation, StyledSlider, Wrapper } from './Slider.styled';
 
 
 interface Props {
@@ -9,14 +10,51 @@ interface Props {
 
 
 function Slider({ children }: Props): JSX.Element {
+    const contentRef = useRef<any>(null);
+
+    const [contentWidth, setContentWidth] = useState(0);
+    const [isNextButtonDisplayed, setIsNextButtonDisplayed] = useState(true);
+    const [isPreviousButtonDisplayed, setIsPreviousButtonDisplayed] = useState(false);
     const [translateValue, setTranslateValue] = useState(0);
+
+    const handleNextClick = () => setTranslateValue(prevState => prevState + 500);
+    const handlePreviousClick = () => setTranslateValue(prevState => prevState - 500);
+
+    useEffect(() => {
+        setContentWidth(contentRef.current?.offsetWidth);
+
+        if (translateValue >= 0 && (translateValue + 200) < contentWidth) {
+            setIsNextButtonDisplayed(true);
+        } else {
+            setIsNextButtonDisplayed(false);
+            setTranslateValue(contentWidth);
+        }
+
+        if (translateValue > 0) {
+            setIsPreviousButtonDisplayed(true);
+        } else {
+            setIsPreviousButtonDisplayed(false);
+            setTranslateValue(0);
+        }
+    }, [contentRef, translateValue]);
 
     return (
         <StyledSlider>
             <Wrapper>
-                <Content translateValue={translateValue}>
+                <Content ref={contentRef} translateValue={translateValue}>
                     {children}
-                    {/** Add logic for Navigation buttons */}
+                    <Navigation>
+                        {isPreviousButtonDisplayed && (
+                            <NavigationButton
+                                isNext={false}
+                                onClick={handlePreviousClick}
+                            />
+                        )}
+
+                        {isNextButtonDisplayed && (
+                            <NavigationButton onClick={handleNextClick} />
+                        )}
+                    </Navigation>
                 </Content>
             </Wrapper>
         </StyledSlider>
