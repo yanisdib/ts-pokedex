8987f65751805b8d280742967a8426afedec5f75
Yanis Dib <ynsprive@gmail.com>
2022-07-10 20:47:14 +0200

    Add useGeneration hook and withStatus HOC

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Hero } from '../../components';
+import { useGeneration } from '../../hooks/useGeneration';
+
+import withStatus from '../../helpers/hocs/withStatus';
+
+import { Generation } from '../../interfaces/Generation';
+
+import { GenerationSection, Hero } from '../../components';
 
 
 function Home(): JSX.Element {
+    const [generationId, setGenerationId] = useState(1);
+
+    const {
+        body: generation,
+        error,
+        status
+    } = useGeneration(generationId);
+
+    const GenerationSectionWithStatus = withStatus<{ generation: Generation }>(GenerationSection);
+
     return (
         <React.Fragment>
             <Hero />
+            <GenerationSectionWithStatus
+                error={error}
+                status={status}
+                {...{ generation }}
+            />
         </React.Fragment>
     );
 }
