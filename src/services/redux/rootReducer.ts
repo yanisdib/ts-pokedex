2909a1effad513b32b484d54223b411088f80a1e
Yanis Dib <ynsprive@gmail.com>
2022-07-10 20:48:33 +0200

    Add new reducer and saga

--- src/services/redux/rootReducer.ts
+++ src/services/redux/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import generationReducer from './slices/generation';
+import generationPokemonReducer from './slices/generationPokemon';
 
 
 const rootReducer = combineReducers({
     generation: generationReducer,
+    generationPokemon: generationPokemonReducer
 });
 
 export type AppState = ReturnType<typeof rootReducer>;
--- src/services/redux/rootSaga.ts
+++ src/services/redux/rootSaga.ts
@@ -1,10 +1,12 @@
 import { all, fork } from 'redux-saga/effects';
 
 import { generationSaga } from './sagas/generation';
+import { generationPokemonSaga } from './sagas/generationPokemon';
 
 
 export default function* rootSaga() {
     yield all([
-        fork(generationSaga)
+        fork(generationSaga),
+        fork(generationPokemonSaga)
     ]);
 }
