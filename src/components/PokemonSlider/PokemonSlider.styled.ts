42d167879108b0dc9cbe9edf2916f671d90799ce
Yanis Dib <ynsprive@gmail.com>
2022-07-10 20:44:04 +0200

    Correct styling issues

--- src/components/PokemonSlider/PokemonSlider.styled.ts
+++ src/components/PokemonSlider/PokemonSlider.styled.ts
@@ -3,12 +3,13 @@ import styled from 'styled-components';
 
 export const StyledPokemonSlider = styled.div`
     display: flex;
-    width: 100%;
+    width: 50%;
     height: fit-content;
+    animation: 0.5s ease-out 0s 1 slideRightToLeft;
 `;
 
 export const Wrapper = styled.div`
     display: flex;
-    width: inherit;
+    width: 100%;
     height: inherit;
 `;
