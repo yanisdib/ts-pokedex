1d714a603e08ed0a5fc93c3bea816afa125238b2
Yanis Dib <ynsprive@gmail.com>
2022-07-10 20:42:02 +0200

    Add useGenerationPokemon hooks and withStatus HOC

--- src/components/GenerationSection/GenerationSection.tsx
+++ src/components/GenerationSection/GenerationSection.tsx
@@ -1,4 +1,10 @@
 import { useState } from 'react';
+
+import { useGenerationPokemon } from '../../hooks/useGenerationPokemon';
+
+import withStatus from '../../helpers/hocs/withStatus';
+
+import { Pokemon } from '../../interfaces/Pokemon';
 import { Generation } from '../../interfaces/Generation';
 
 import PokemonSlider from '../PokemonSlider/PokemonSlider';
@@ -10,6 +16,9 @@ import {
     Wrapper
 } from './GenerationSection.styled';
 
+import generationBanner from '../../assets/images/bg-gen-i.jpg';
+
+
 
 interface Props {
     generation: Generation;
@@ -19,19 +28,34 @@ interface Props {
 function GenerationSection({ generation }: Props): JSX.Element {
     const [isBannerLoaded, setIsBannerLoaded] = useState(false);
 
+    const { id, main_region, pokemon_species } = generation;
+
+    const {
+        body: generationPokemon,
+        error,
+        status
+    } = useGenerationPokemon(pokemon_species);
+
+    const PokemonSliderWithStatus = withStatus<{ pokemon: Pokemon[] }>(PokemonSlider);
+
     return (
         <Section>
             <Wrapper>
                 <Content>
+                    <h1>{`Generation ${id} from ${main_region.name}`}</h1>
                     <Illustration loaded={isBannerLoaded}>
                         {/** Dummy data until logic is implemented */}
                         <img
                             alt="Illustration by Nintendo that shows a few Pokémon from the first generation like Pikachu, Pidgey, Rattata and their trainers"
                             onLoad={() => setIsBannerLoaded(true)}
-                            src=""
+                            src={generationBanner}
                         />
                     </Illustration>
-                    <PokemonSlider />
+                    <PokemonSliderWithStatus
+                        error={error}
+                        status={status}
+                        {...{ pokemon: generationPokemon }}
+                    />
                 </Content>
             </Wrapper>
         </Section>
